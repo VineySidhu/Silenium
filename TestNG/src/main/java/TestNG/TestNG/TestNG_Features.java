a771296a854d792eda871c482c967bedadc1f0ae
esidvin <esidvin@IN-00208619.ericsson.se>
2020-02-12 12:00:11 +0530

    go

---
+++ TestNG/src/main/java/TestNG/TestNG/TestNG_Features.java
@@ -0,0 +1,17 @@
+package TestNG.TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Features {
+	@Test
+	public void LoginTest()
+	{
+		System.out.println("Login Test");
+	}
+	@Test
+	public void HomePageTest()
+	{
+		System.out.println("HomePageTest");
+	}
+
+}
